6ce9c8e1b9a8f4bc74869bc7bbf540c01d11f596
galihmrd <galihmardiana15@gmail.com>
2023-04-14 02:23:32 +0700

    Revert "test fix again"
    This reverts commit dffadb7e4c8b5f2ee17cf57a84aa82244e03c9d3.

--- src/misc.js
+++ src/misc.js
@@ -67,16 +67,14 @@ module.exports = sansekai = async (client, m, chatUpdate, store) => {
         }
     } else if (m.text.startsWith("!block")) {
         if (number === setting.ownerNumber) {
-            const [result] = await sock.onWhatsApp(m.text.split("!block ")[1])
-            client.updateBlockStatus(result.jid + "@s.whatsapp.net", "block")
-            m.reply(m.text.split("!block ")[1] + " Blocked!")
+            client.updateBlockStatus(m.text.split("!block ")[1] + "@s.whatsapp.net", "block")
+            m.reply("+" + m.text.split("!block ")[1] + " Blocked!")
         } else {
             m.reply("Owner only!")
         }
     } else if (m.text.startsWith("!unblock")) {
         if (number === setting.ownerNumber) {
-            const [result] = await sock.onWhatsApp(m.text.split("!unblock ")[1])
-            client.updateBlockStatus(m.text.split(result.jid + "@s.whatsapp.net", "unblock")
+            client.updateBlockStatus("+" + m.text.split("!unblock ")[1] + "@s.whatsapp.net", "unblock")
             m.reply(m.text.split("!unblock ")[1] + " Unblocked!")
         } else {
             m.reply("Owner only!")
